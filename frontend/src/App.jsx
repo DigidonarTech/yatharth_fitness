1a4c2335d21072af4026779910f6c5ae8cb38774
jitendra kumar <jitendrakumarjjk10@gmail.com>
2026-05-12 17:36:07 +0530

    website architecture and folder structure has been created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+      <h1>Welcome to the Yatharth Fitness!</h1>
+      <p>Get ready to embark on a fitness journey like no other. At Yatharth Fitness, we are dedicated to helping you achieve your health and wellness goals with personalized training programs, expert guidance, and a supportive community. Whether you're a beginner or a seasoned athlete, our state-of-the-art facilities and passionate trainers are here to empower you every step of the way. Join us today and let's work together to unlock your full potential and transform your life through fitness!</p>
+    </>
+  )
+}
+
+export default App
